011b45e1fa96841b63791bad3360ebbcb133734a
Iurii Piurbeev <ypurga@gmail.com>
2019-07-21 19:47:06 +0200

    Create two-sum.cpp

---
+++ two-sum.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/two-sum/
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> m;
+        for (int i = 0; i < nums.size(); ++i) {
+            // invariant:
+            // all previous values are in "m" - mapping to indexes
+            // gonna check if current value makes a sum with any of them
+            auto lookup = m.find(target - nums[i]);
+            if (lookup != m.end()) {
+                return vector<int> { lookup->second, i };
+            }
+            
+            // just keep the invariant
+            m[nums[i]] = i;
+        }
+        
+        // they promised exactly one solution always
+        return vector<int>();
+    }
+};
